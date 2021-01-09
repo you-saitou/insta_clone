c9b96a94111c6d435bcddb90066565a79afd5775
yuu saito <sublimation214@gmail.com>
2021-01-09 16:08:49 +0900

    ✨ Change root depending on login status
    ログイン状態によってrootを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  constraints ->  request { request.session[:user_id].present? } do
+    # ログインしてる時のルートパス
+    root 'posts#index'
+  end
+  # ログインしてない時のルートパス
+  root 'user_sessions#new'
 
-  resources :users, only: %i[new create]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
+  resources :posts
 end
