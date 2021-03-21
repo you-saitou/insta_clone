317b2742bcf6bcd03e209310edac3cd8c3e6721a
yuu saito <sublimation214@gmail.com>
2021-03-21 14:31:47 +0900

    ✨ dealing with the N+1 problem
    N+1問題の対応

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_login, only: %i[new create edit update destroy]
   def index
-    @posts = Post.all
+    @posts = Post.all.includes(:user)
   end
 
   def new
